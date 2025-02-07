87d3a8e82b793f33acc8c635584ee74abe2a5c81
KyuzenDev <antcraft517@gmail.com>
2025-02-07 11:45:16 +0700

    07/02/2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Trusted by companies like Uber, Amazon, eBay, and Walmart</p>
+      <p></p>
     </footer>
   );
 };
